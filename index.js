8812dd3d61f0db6d9bbf29324a16a0c5356e9b96
jerryblack0315 <b03505006@ntu.edu.tw>
2017-03-14 10:49:51 +0800

    chat-example

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/bdex.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', msg);
+    //io.emit('some event', { for: 'everyone' });
+    //io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
