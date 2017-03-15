9602b4fffb61f7eced9a0770e45c0d19063b221a
BobML2017 <b03901164@ntu.edu.tw>
2017-03-15 15:15:38 +0800

    input username

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/bdex.html');
+  res.sendFile(__dirname + '/client.html');
 });
 
 io.on('connection', function(socket){
+  var id
   socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg, id);
     //io.emit('some event', { for: 'everyone' });
     //io.emit('chat message', msg);
+    socket.emit('chat message', msg, id);
     console.log('message: ' + msg);
+  });
+  socket.on('addme', function(username){
+    id=username;
+    socket.emit('check id', username, 'You are connect.');
+    socket.broadcast.emit('chat message', username + ' is on deck.', '');
   });
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
